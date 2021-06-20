4b8e2c169eaef0673376cd5156f14b59544e3273
Levendor <myth_of_lavanda@mail.ru>
2021-06-20 14:28:28 +0300

    feat: add connection options

--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -1,10 +1,13 @@
+import { ConnectionOptions } from 'typeorm';
 import { DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } from './config';
 
-export default {
+export const PORT: number = Number(DB_PORT) || 4000;
+
+export const connectionOptions: ConnectionOptions = {
   name: 'default',
   type: 'postgres',
   host: 'postgres',
-  port: DB_PORT,
+  port: PORT,
   database: DB_NAME,
   username: DB_USER,
   password: DB_PASSWORD,
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
+import { createConnection } from 'typeorm';
+
 import { PORT } from './common/config';
+import { connectionOptions } from './common/ormconfig'
 import { createApp } from './app';
+import { logger } from './common/logger';
 
-const app = createApp();
+export const APP_PORT: number = Number(PORT) || 4000;
 
-app.listen(PORT, () =>
-  process.stdout.write(`App is running on http://localhost:${PORT}\n`)
-);
+const onConnect = () => {
+  createApp().listen(APP_PORT, () =>
+  logger('info', 'App is running on', { URL: `http://localhost:${APP_PORT}` })
+  );
+}
+
+const onError = (error: Error) => {
+  const { message, stack } = error;
+  logger('error', 'Unable to connect to db', { message, stack });
+  process.exit(1);
+}
+
+createConnection(connectionOptions)
+  .then(onConnect)
+  .catch(onError);
