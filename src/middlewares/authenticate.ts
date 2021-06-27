64f5a56c7e2b9805e4a26dcaea8461125e0db688
Levendor <myth_of_lavanda@mail.ru>
2021-06-27 03:55:29 +0300

    fix: extract bearer token from bearer schema

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -7,11 +7,12 @@ import { JWT_SECRET_KEY } from '../common/config';
 const { UNAUTHORIZED } = StatusCodes;
 
 export const authenticate: RequestHandler = (req, _res, next) => {
-  const token = req.headers.authorization;
+  const bearerToken = req.headers.authorization;
   const secretKey = JWT_SECRET_KEY || 'secret-key';
 
-  if (token) {
-    verify(token.slice(7), secretKey, (err) => {
+  if (bearerToken) {
+    const token = bearerToken.slice(7);
+    verify(token, secretKey, (err) => {
       if (err) {
         const error = newError(UNAUTHORIZED, 'Failed to authenticate token!');
         error.stack = err.stack;
